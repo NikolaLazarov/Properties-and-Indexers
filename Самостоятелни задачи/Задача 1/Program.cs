1f876f7d15cca38d670c1cedf4ad153e4c21a5a9
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-03-01 20:01:53 +0200

    Add files via upload

---
+++ Самостоятелни задачи/Задача 1/Program.cs
@@ -0,0 +1,37 @@
+﻿using static System.Console;
+class myClass
+{
+    private char s;
+    public char charater
+    {
+        get
+        {
+            return s;
+        }
+        set
+        {
+            if ((int)value < 65 || (int)value > 90)
+            {
+                WriteLine("Please enter a charater between A and Z");
+            }
+            else
+            {
+                s = value;
+            }
+        }
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        myClass obj = new myClass();
+
+        obj.charater = 'B';
+        WriteLine(obj.charater);
+        obj.charater = '@';
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
---
+++ Самостоятелни задачи/Задача 2/Program.cs
@@ -0,0 +1,39 @@
+﻿using static System.Console;
+class myClass
+{
+    private int[] nums;
+    int p;
+    public myClass(int n)
+    {
+        p = 1;
+        for (int i = 0; i <= n; i++) { p *= 10; }
+        nums = new int[n];
+    }
+    public override string ToString()
+    {
+        string str = "";
+        for (int i = 0; i < nums.Length; i++)
+        {
+            str += nums[i] + " ";
+        }
+        return str;
+    }
+    public int this[int i]
+    {
+        set { nums[i] = value; }
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        myClass obj = new myClass(4);
+        WriteLine(obj);
+        obj[1] = 6;
+        obj[2] = 9;
+        WriteLine(obj);
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
